acc658f48932c51555d98c2c966ad08cd448f7ed
Hardy Griech <ntbox@gmx.net>
2023-03-11 10:39:21 +0100

    boilerplate for rp2040 & gcc

---
+++ llvm-games/boilerplate/rp2040/main.cpp
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <pico/stdlib.h>
+#ifdef CYW43_LWIP
+    #include "pico/cyw43_arch.h"
+#endif
+
+
+
+
+static void led(uint8_t state)
+{
+#ifdef CYW43_LWIP
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, state);
+#elif defined(PICOPROBE_LED)
+    static bool initialized;
+
+    if ( !initialized) {
+        initialized = true;
+        gpio_init(PICOPROBE_LED);
+        gpio_set_dir(PICOPROBE_LED, GPIO_OUT);
+    }
+    gpio_put(PICOPROBE_LED, state);
+#endif
+}   // led
+
+
+
+int main()
+{
+#ifdef CYW43_LWIP
+    if (cyw43_arch_init()) {
+        //printf("failed to initialize WiFi\n");
+    }
+#endif
+    while (true) {
+        led(1);
+        sleep_ms(250);
+        led(0);
+        sleep_ms(250);
+    }
+}
