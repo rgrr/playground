6245e287a8566d641ef35dbfbd99efa9a2a9994e
Hardy Griech <ntbox@gmx.net>
2023-03-16 11:44:48 +0100

    it seems that openocd can do file IO while pyocd cannot

--- llvm-games/profiling/main.cpp
+++ llvm-games/profiling/main.cpp
@@ -6,13 +6,29 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <fcntl.h>
 
 
 
-// this results in "high" speed output on the gdb console with about "one line / 2s"
+__attribute__ ((format (printf, 1, 2))) void _printf(const char *format, ...)
+ /**
+  * With this printf helper semihosting output is much faster because then blocks and not characters
+  * are written.
+  */
+{
+    static char buf[128];
+    va_list arglist;
 
-extern "C" void __llvm_profile_write_buffer(uint8_t *);
-uint8_t buff[4096];
+    va_start(arglist, format);
+    vsnprintf(buf, sizeof(buf), format, arglist);
+    va_end(arglist);
+    write(STDOUT_FILENO, buf, strlen(buf));
+}   // _printf
+
 
 
 int main()
@@ -20,7 +36,31 @@ int main()
     uint32_t cnt = 0;
 
     for (int i = 0;  i < 2;  ++i) {
-        printf("-- %u\n", cnt++);
+        _printf("-- %u\n", cnt++);
     }
-    __llvm_profile_write_buffer(buff);
-}
+
+    static const uint8_t buf[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+#if 0
+    FILE *f;
+    size_t r;
+
+    f = fopen("test.bin", "wb");
+    _printf("f=%p\n", f);
+    r = fwrite(buf, sizeof(buf), 1, f);
+    _printf("r=%d\n", r);
+    r = fclose(f);
+    _printf("r=%d\n", r);
+#else
+    int f;
+    int r;
+
+    write(STDOUT_FILENO, "Aber Hallo\n", 11);
+
+    f = open("llvm-games/profiling/test.binx", O_WRONLY | O_APPEND | O_CREAT, 0644);
+    _printf("f=%d\n", f);
+    r = write(f, buf, sizeof(buf));
+    _printf("r=%d\n", r);
+    r = close(f);
+    _printf("r=%d\n", r);
+#endif
+}   // main
