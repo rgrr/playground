800c5b91a1e8ac2527165cacc06e550d9b52a63e
Hardy Griech <ntbox@gmx.net>
2023-03-17 13:03:56 +0100

    taking input from smithp35 into account, esp registering

--- llvm-games/profiling/profiling/InstrProfilingFileOther.c
+++ llvm-games/profiling/profiling/InstrProfilingFileOther.c
@@ -1,10 +1,15 @@
+/*===- InstrProfilingFileOther.c - Write instrumentation to a file --------===*\
+|*
+|* Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+|* See https://llvm.org/LICENSE.txt for license information.
+|* SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+|*
+\*===----------------------------------------------------------------------===*/
 
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
-
 #include <fcntl.h>
-
 
 #include "InstrProfiling.h"
 #include "InstrProfilingInternal.h"
@@ -15,7 +20,159 @@
 
 #define FILE_NAME   "default.profraw"
 
+static void writeFileWithoutReturn(void);
 
+
+//----------------------------------------------------------------------------------------------------------------------
+//
+// In the original sources the following functions reside in InstrProfilingPlatformOther.c
+// Changes:
+// - register writeFileWithoutReturn() with atexit()
+// - skip function which tries to register INSTR_PROF_PROFILE_RUNTIME_VAR
+//
+
+static const __llvm_profile_data *DataFirst = NULL;
+static const __llvm_profile_data *DataLast = NULL;
+static const char *NamesFirst = NULL;
+static const char *NamesLast = NULL;
+static char *CountersFirst = NULL;
+static char *CountersLast = NULL;
+
+#define INSTR_PROF_PROFILE_RUNTIME_VAR __llvm_profile_runtime
+int INSTR_PROF_PROFILE_RUNTIME_VAR;
+
+
+static inline const void *getMinAddr(const void *A1, const void *A2)
+{
+    return A1 < A2 ? A1 : A2;
+}
+
+
+static inline const void *getMaxAddr(const void *A1, const void *A2)
+{
+    return A1 > A2 ? A1 : A2;
+}
+
+
+// Given a pointer to the __llvm_profile_data for the function, record the
+// bounds of the profile data and profile count sections.
+// This function is called several time by the __llvm_profile_init function
+// at program start.
+//
+// If this function is called we register __llvm_profile_dump() with
+// atexit to write out the profile information to file.
+void __llvm_profile_register_function(void *Data_)
+{
+    static int RegisteredDumper = 0;
+    const __llvm_profile_data *Data = (__llvm_profile_data *)Data_;
+
+    if ( !RegisteredDumper)
+    {
+        RegisteredDumper = 1;
+        atexit(writeFileWithoutReturn);
+    }
+
+    // The __llvm_profile_init function in each .o file first
+    // calls __llvm_profile_register_function with a pointer
+    // to the INSTR_PROF_PROFILE_RUNTIME_VAR. In some runtime
+    // implementation this might provoke some action, but for
+    // us it is a no-op. We must return before trying to
+    // interpret the contents as __llvm_profile_data.
+    if (Data == (void*)&INSTR_PROF_PROFILE_RUNTIME_VAR)
+    {
+#ifdef PROFLIB_DEBUG
+        fprintf(stderr, "skipping __llvm_profile_register_function for llvm_profile_runtime\n");
+#endif // PROFLIB_DEBUG
+        return;
+    }
+#ifdef PROFLIB_DEBUG
+    fprintf(stderr, "__llvm_profile_register_function Data_ %p\n", Data);
+    fprintf(stderr, "  counters: %u at %p\n", Data->NumCounters,
+            Data->CounterPtr);
+#endif // PROFLIB_DEBUG
+
+    if ( !DataFirst)
+    {
+        DataFirst = Data;
+        DataLast = Data + 1;
+        CountersFirst = (char *)((uintptr_t)Data_ + Data->CounterPtr);
+        CountersLast = CountersFirst + Data->NumCounters * __llvm_profile_counter_entry_size();
+        return;
+    }
+
+    DataFirst = (const __llvm_profile_data *)getMinAddr(DataFirst, Data);
+    CountersFirst = (char *)getMinAddr(CountersFirst, (char *)((uintptr_t)Data_ + Data->CounterPtr));
+
+    DataLast = (const __llvm_profile_data *)getMaxAddr(DataLast, Data + 1);
+    CountersLast = (char *)getMaxAddr(CountersLast, (char *)((uintptr_t)Data_ + Data->CounterPtr) +
+                                                    Data->NumCounters * __llvm_profile_counter_entry_size());
+}   // __llvm_profile_register_function
+
+
+void __llvm_profile_register_names_function(void *NamesStart, uint64_t NamesSize)
+{
+#ifdef PROFLIB_DEBUG
+    fprintf(stderr, "__llvm_profile: register names %p length %" PRIu64 "\n",
+            NamesStart, NamesSize);
+#endif // PROFLIB_DEBUG
+
+    if (!NamesFirst)
+    {
+        NamesFirst = (const char *)NamesStart;
+        NamesLast = (const char *)NamesStart + NamesSize;
+        return;
+    }
+    NamesFirst = (const char *)getMinAddr(NamesFirst, NamesStart);
+    NamesLast = (const char *)getMaxAddr(NamesLast, (const char *)NamesStart + NamesSize);
+}   // __llvm_profile_register_names_function
+
+
+const __llvm_profile_data *__llvm_profile_begin_data(void)
+{
+    return DataFirst;
+}   // __llvm_profile_begin_data
+
+
+const __llvm_profile_data *__llvm_profile_end_data(void)
+{
+    return DataLast;
+}   // __llvm_profile_end_data
+
+
+int __llvm_write_binary_ids(ProfDataWriter *Writer)
+{
+    return 0;
+}   // __llvm_write_binary_ids
+
+
+const char *__llvm_profile_begin_names(void)
+{
+    return NamesFirst;
+}   // __llvm_profile_begin_names
+
+
+const char *__llvm_profile_end_names(void)
+{
+    return NamesLast;
+}   // __llvm_profile_end_names
+
+
+char *__llvm_profile_begin_counters(void)
+{
+    return CountersFirst;
+}   // __llvm_profile_begin_counters
+
+
+char *__llvm_profile_end_counters(void)
+{
+    return CountersLast;
+}   // __llvm_profile_end_counters
+
+
+//----------------------------------------------------------------------------------------------------------------------
+//
+// Actual dumper is very simple.  Idea is to use semihosting for file IO, but if other functions
+// are provided, the dumper is still happy.
 
 /* Return 1 if there is an error, otherwise return  0.  */
 static uint32_t fileWriter(ProfDataWriter *This, ProfDataIOVec *IOVecs, uint32_t NumIOVecs)
@@ -79,34 +236,7 @@ int __llvm_profile_write_file(void)
 
 
 
-/**
- * This method is invoked by the runtime initialization hook
- * InstrProfilingRuntime.o if it is linked in.
- */
-COMPILER_RT_VISIBILITY void __llvm_profile_initialize(void)
-{
-    __llvm_profile_initialize_file();
-    __llvm_profile_register_write_file_atexit();
-}   // __llvm_profile_initialize
-
-
-
 static void writeFileWithoutReturn(void)
 {
     __llvm_profile_write_file();
 }   // writeFileWithoutReturn
-
-
-
-COMPILER_RT_VISIBILITY int __llvm_profile_register_write_file_atexit(void)
-{
-    static bool HasBeenRegistered = false;
-
-    if (HasBeenRegistered)
-        return 0;
-    HasBeenRegistered = true;
-
-    return atexit(writeFileWithoutReturn);
-}   // __llvm_profile_register_write_file_atexit
-
-
