06ee397e2b60024e13ed11f54fac5ef10235bdfd
Hardy Griech <ntbox@gmx.net>
2023-05-14 09:51:34 +0200

    updates README

--- tools/SystemView/main.c
+++ tools/SystemView/main.c
@@ -106,6 +106,34 @@ void SEGGER_SYSVIEW_Conf(void)
 
 
 
+//======================================================================================================================
+#include "SEGGER_RTT.h"
+
+static int rtt_putc(char c, FILE *file)
+{
+    (void) file;
+    SEGGER_RTT_Write(0, &c, 1);
+    return c;
+}   // rtt_putc
+
+static int rtt_getc(FILE *file)
+{
+    (void) file;
+    uint8_t buf;
+    unsigned r = SEGGER_RTT_Read(0, &buf, sizeof(buf));
+    return (r == 0) ? EOF : buf;
+}   // rtt_getc
+
+static FILE __stdio = FDEV_SETUP_STREAM(rtt_putc, rtt_getc, NULL, _FDEV_SETUP_RW);
+
+//FILE *const stdout = &__stdio;
+FILE *const stdin = &__stdio;
+__strong_reference(stdin, stdout);
+__strong_reference(stdin, stderr);
+//======================================================================================================================
+
+
+
 #define MARKER_PRINT   0x2222
 #define TASKID_PRINT   0x22
 #define TASKID_DELAY   0x11
@@ -139,34 +167,6 @@ static void PrintCycCnt(int i)
     }
     SEGGER_SYSVIEW_OnTaskStopExec();
 }   // PrintC
-
-
-
-//======================================================================================================================
-#include "SEGGER_RTT.h"
-
-static int rtt_putc(char c, FILE *file)
-{
-    (void) file;
-    SEGGER_RTT_Write(0, &c, 1);
-    return c;
-}   // rtt_putc
-
-static int rtt_getc(FILE *file)
-{
-    (void) file;
-    uint8_t buf;
-    unsigned r = SEGGER_RTT_Read(0, &buf, sizeof(buf));
-    return (r == 0) ? EOF : buf;
-}   // rtt_getc
-
-static FILE __stdio = FDEV_SETUP_STREAM(rtt_putc, rtt_getc, NULL, _FDEV_SETUP_RW);
-
-//FILE *const stdout = &__stdio;
-FILE *const stdin = &__stdio;
-__strong_reference(stdin, stdout);
-__strong_reference(stdin, stderr);
-//======================================================================================================================
 
 
 
