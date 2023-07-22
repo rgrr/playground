d0f2bc81b52b70e79f928151bc5e51c2ba149d7d
Hardy Griech <ntbox@gmx.net>
2023-07-22 10:36:07 +0200

    stdio to RTT is contained in library

--- llvm-games/SystemView/main.c
+++ llvm-games/SystemView/main.c
@@ -14,6 +14,7 @@
 
 #include "nrf.h"
 
+#include "SEGGER/RTT/SEGGER_RTT.h"
 #include "SEGGER/RTT/SEGGER_SYSVIEW.h"
 
 
@@ -69,34 +70,6 @@ void CycCnt_Init(void)
         }
     }
 }   // CycCnt_Init
-
-
-//======================================================================================================================
-#include "SEGGER/RTT/SEGGER_RTT.h"
-
-static int rtt_putc(char c, FILE *file)
-{
-    (void) file;
-    SEGGER_RTT_Write(0, &c, 1);
-    return c;
-}   // rtt_putc
-
-static int rtt_getc(FILE *file)
-{
-    (void) file;
-    uint8_t buf;
-    unsigned r = SEGGER_RTT_Read(0, &buf, sizeof(buf));
-    return (r == 0) ? EOF : buf;
-}   // rtt_getc
-
-static FILE __stdio = FDEV_SETUP_STREAM(rtt_putc, rtt_getc, NULL, _FDEV_SETUP_RW);
-
-//FILE *const stdout = &__stdio;
-FILE *const stdin = &__stdio;
-__strong_reference(stdin, stdout);
-__strong_reference(stdin, stderr);
-//======================================================================================================================
-
 
 
 /*********************************************************************
