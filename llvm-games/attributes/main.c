98cf00447c4273d1c7406f1c9459b23709b7a580
Hardy Griech <ntbox@gmx.net>
2023-08-06 07:58:14 +0200

    main "enhancements"

--- llvm-games/attributes/main.c
+++ llvm-games/attributes/main.c
@@ -9,6 +9,58 @@
 #include <SEGGER/RTT/SEGGER_RTT.h>
 
 
+#define xINC_HANDLER
+#define xINC_CONST
+#define INC_CONST_TINYUSB
+
+
+//----------------------------------------------------------------------------------------------------------------------
+//
+// misc utilities
+//
+extern uint32_t __stack;
+extern uint32_t __my_data_end;
+
+
+static void _Delay_us(uint32_t period_us)
+{
+    volatile int i;
+
+    i = 6400 * (period_us / 1000);
+    do {
+        __asm volatile ("nop\n" : : :);
+    } while (i--);
+}   // _Delay_us
+
+
+static char const *p_eval( uint8_t const * const p )
+{
+    if (p >= (uint8_t *)&__my_data_end) {
+        return "STACK";
+    }
+    else if (p >= (uint8_t *)0x20000000) {
+        return "RAM";
+    }
+    return "FLASH";
+}
+
+
+#if !defined(__clang_major__)
+// actually required for newlib
+void _exit(void)
+{
+    for (;;) {
+    }
+}   // _exit
+#endif
+
+
+//----------------------------------------------------------------------------------------------------------------------
+//
+// various function attributes
+//
+#if defined(INC_HANDLER)
+
 typedef void (*VECTOR_FUNCTION_Type)(void);
 
 void Interrupt_Handler(void) __attribute__ ((interrupt ("IRQ")));
@@ -48,27 +100,27 @@ void Noreturn_Handler(void)
 }
 
 
-#define INC_CONST
+static void print_functions_addresses(void)
+{
+    SEGGER_RTT_printf(0, "Interrupt_Handler   : %p\n", Interrupt_Handler);
+    SEGGER_RTT_printf(0, "Noattribute_Handler : %p\n", Noattribute_Handler);
+    SEGGER_RTT_printf(0, "Naked_Handler       : %p\n", Naked_Handler);
+    SEGGER_RTT_printf(0, "Noreturn_Handler    : %p\n", Noreturn_Handler);
+}   // print_functions_addresses
 
+#endif
+
+
+//----------------------------------------------------------------------------------------------------------------------
+//
+// various const definitions and the corresponding memory layout
+//
 #ifdef INC_CONST
+
 uint8_t u0[]              = "uint8_t []";
 static uint8_t u1[]       = "static uint8_t *";
 const uint8_t u2[]        = "const uint8_t *";
 static const uint8_t u3[] = "static const uint8_t *";
-extern uint32_t __stack;
-extern uint32_t __my_data_end;
-
-
-static char const *p_eval( uint8_t const * const p )
-{
-    if (p >= (uint8_t *)&__my_data_end) {
-        return "STACK";
-    }
-    else if (p >= (uint8_t *)0x20000000) {
-        return "RAM";
-    }
-    return "FLASH";
-}
 
 
 static void p_u( uint8_t *p )
@@ -90,30 +142,52 @@ static void constp_constu( uint8_t const * const p )
 {
     SEGGER_RTT_printf(0, "  constp_constu('%s')=0x%08p - %s\n", (char *)p, p, p_eval(p));
 }
+
+static void print_const_memory_layout(void)
+{
+    _Delay_us(100 * 1000);
+    p_u(u0);
+    p_u(u1);
+
+    _Delay_us(100 * 1000);
+    constp_u(u0);
+    constp_u(u1);
+
+    _Delay_us(100 * 1000);
+    p_constu(u0);
+    p_constu(u1);
+    p_constu(u2);
+    p_constu(u3);
+
+    _Delay_us(100 * 1000);
+    constp_constu(u0);
+    constp_constu(u1);
+    constp_constu(u2);
+    constp_constu(u3);
+}   // print_const_memory_layout
+
 #endif
 
 
+//----------------------------------------------------------------------------------------------------------------------
+//
+// various function attributes
+//
+#if defined(INC_CONST_TINYUSB)
 
-static void _Delay_us(uint32_t period_us)
+uint8_t tud_network_mac_address[6] = {0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff };
+const uint8_t const_tud_network_mac_address[6] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66};
+
+static void print_tinyusb_memory_layout(void)
 {
-    volatile int i;
-
-    i = 6400 * (period_us / 1000);
-    do {
-        __asm volatile ("nop\n" : : :);
-    } while (i--);
-}   // _Delay_us
-
-
-
-#if !defined(__clang_major__)
-// actually required for newlib
-void _exit(void)
-{
-    for (;;) {
-    }
-}   // _exit
+    _Delay_us(100 * 1000);
+    SEGGER_RTT_printf(0, "tud_network_mac_address=0x%08p - %s\n", tud_network_mac_address, p_eval(tud_network_mac_address));
+    SEGGER_RTT_printf(0, "const_tud_network_mac_address=0x%08p - %s\n", const_tud_network_mac_address, p_eval(const_tud_network_mac_address));
+}   // print_tinyusb_memory_layout
 #endif
+
+
+//----------------------------------------------------------------------------------------------------------------------
 
 
 int main()
@@ -144,34 +218,20 @@ int main()
     SEGGER_RTT_printf(0, "Stack               : %p\n", &__stack);
     SEGGER_RTT_printf(0, "Data End            : %p\n", &__my_data_end);
     SEGGER_RTT_printf(0, "----------------------------------------\n");
-    SEGGER_RTT_printf(0, "Interrupt_Handler   : %p\n", Interrupt_Handler);
-    SEGGER_RTT_printf(0, "Noattribute_Handler : %p\n", Noattribute_Handler);
-    SEGGER_RTT_printf(0, "Naked_Handler       : %p\n", Naked_Handler);
-    SEGGER_RTT_printf(0, "Noreturn_Handler    : %p\n", Noreturn_Handler);
+#if defined(INC_HANDLER)
+    print_functions_addresses();
     SEGGER_RTT_printf(0, "========================================\n");
+#endif
 
 #ifdef INC_CONST
-    _Delay_us(200 * 1000);
-    p_u(u0);
-    p_u(u1);
-
-    _Delay_us(200 * 1000);
-    constp_u(u0);
-    constp_u(u1);
-
-    _Delay_us(200 * 1000);
-    p_constu(u0);
-    p_constu(u1);
-    p_constu(u2);
-    p_constu(u3);
-
-    _Delay_us(200 * 1000);
-    constp_constu(u0);
-    constp_constu(u1);
-    constp_constu(u2);
-    constp_constu(u3);
-#endif
+    print_const_memory_layout();
     SEGGER_RTT_printf(0, "========================================\n");
+#endif
+
+#if defined(INC_CONST_TINYUSB)
+    print_tinyusb_memory_layout();
+    SEGGER_RTT_printf(0, "========================================\n");
+#endif
 
     while (true) {
         _Delay_us(250 * 1000);
